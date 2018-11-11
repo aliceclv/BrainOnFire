b9c3a8f919b54f3e08615707c5f2853393a27eb9
Alice Clavel <alice.claouis@gmail.com>
2018-11-11 18:12:54 +0100

    feat: proper redux setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
@@ -19,3 +20,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+Expo.registerRootComponent(App);
